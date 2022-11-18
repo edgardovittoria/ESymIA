8c7292ee716651e3ed0e962ea3b5a262d641dad1
edgardovittoria <edgardovittoria@hotmail.it>
2022-11-18 11:15:17 +0100

    created sharingInfo slice

---
+++ src/store/sharingInfoSlice.ts
@@ -0,0 +1,27 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+export type SharingInfoState = {
+
+}
+
+export const SharingInfoSlice = createSlice({
+    name: 'sharingInfoSlice',
+    initialState: {
+
+    } as SharingInfoState,
+    reducers: {
+        //qui vanno inserite le azioni
+    },
+    extraReducers: {
+        //qui inseriamo i metodi : PENDING, FULLFILLED, REJECT utili per la gestione delle richieste asincrone
+}
+})
+
+
+export const {
+    //qui vanno inserite tutte le azioni che vogliamo esporatare
+
+} = SharingInfoSlice.actions
+
+export const StateSelector = (state: { : SharingInfoState }) => state.;
